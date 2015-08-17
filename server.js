6cca2b7847de235a3ebf7eb4e0702328bd485a7b
Thômas Rodrigues <tfrodrigues@gmail.com>
2015-08-17 16:25:53 -0300

    Ajuste na inicialização do server

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ var bodyParser = require('body-parser');
 var browserify = require('browserify-middleware');
 var utils = require('./js/common/utils');
 
-var renderPage = function(res, name, path, query) {
+var renderPage = function(res, name, path, query, equipe) {
     res.render(path + '/pages/' + name + '.html', {
         name: name,
         path: path,
-        query: query
+        query: query,
+        equipe: JSON.stringify(equipe)
     });
 };
 
 var app = express();
-
-var fs = require('fs'); 
 
 app.set('view engine', 'html');
 
@@ -24,8 +23,6 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(__dirname + '/images'));
 
-app.use('/styles/css/bootstrap', express.static(__dirname + '/libs/bootstrap/dist/css'));
-
 app.use('/styles', express.static(__dirname + '/styles'));
 
 app.use('/js', browserify(__dirname + '/js/entry-points'));
@@ -34,43 +31,18 @@ app.get('/', function (req, res) {
     res.redirect('/views/home');
 });
 
-app.get('/views/product/:className?/:productName?', function (req, res) {
-    var className = req.params.className;
-    var productName = req.params.productName;
-    var name = (className && productName) ? 'product' : 'product-list';
-
-    var query = {};
-    if (className) {
-        query['key'] = className;
-    }
-    if (productName) {
-        query['items.key'] = productName;
-    }
-
-    renderPage(res, name, 'site', query);
-});
-
 app.get('/views/:name', function (req, res) {
     renderPage(res, req.params.name, 'site', req.query);
 });
 
 app.get('/:pagina', function(req,res) {
     db['Equipe'].Model.findOne({ pagina: req.params.pagina}, function (err, value){
-    if (err) {
-        res.status(404).send(err);
-    } else {
-        res.render('site/pages/equipe.html', {
-        path: 'site',
-        name: 'equipe',
-        query: req.query,
-        equipe: value
-        });
-    }
+        if (err) {
+            res.status(404).send(err);
+        } else {
+            renderPage(res, 'equipe', 'site', req.query, value);
+        }
     });
-});
-
-app.get('/cms/views/:name', function (req, res) {
-    renderPage(res, req.params.name, 'admin', req.query);
 });
 
 app.get('/:name/find', function (req, res) {
@@ -85,30 +57,6 @@ app.get('/:name/find', function (req, res) {
             res.status(404).send(err);
         } else {
             res.send(values);
-        }
-    });
-});
-
-app.post('/:name/save', function (req, res) {
-    var name = utils.capitalize(req.params.name);
-    db.saveOrUpdate(db[name].Model, req.body, function (err, value) {
-        if (err) {
-            res.status(404).send(err);
-        } else {
-            res.send(value);
-        }
-    });
-});
-
-app.delete('/:name/remove/:id', function (req, res) {
-    var name = utils.capitalize(req.params.name);
-    var id = req.params.id;
-
-    db.remove(db[name].Model, id, function (err, value) {
-        if (err) {
-            res.status(404).send(err);
-        } else {
-            res.send(value);
         }
     });
 });
