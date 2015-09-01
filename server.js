4516831cac5bdca604f9f551f23e1aad05d69527
Thômas Rodrigues <tfrodrigues@gmail.com>
2015-09-01 10:20:49 -0300

    Adicionado controle de cache para chamadas express

--- server.js
+++ server.js
@@ -6,36 +6,39 @@ var utils = require('./js/common/utils');
 var cookieParser = require('cookie-parser');
 var cryptoJS = require('crypto-js');
 var compression = require('compression');
+var cacheResponseDirective = require('express-cache-response-directive');
 
 var renderPage = function(res, name, path, query, logged, loggedOnPage, equipe) {
-    res.render(path + '/pages/' + name + '.html', {
-        name: name,
-        path: path,
-        query: query,
-        logged: logged,
-        loggedOnPage: loggedOnPage,
-        equipe: JSON.stringify(equipe)
-    });
+   res.cacheControl({maxAge: oneMonth});
+   res.render(path + '/pages/' + name + '.html', {
+    name: name,
+    path: path,
+    query: query,
+    logged: logged,
+    loggedOnPage: loggedOnPage,
+    equipe: JSON.stringify(equipe)
+});
 };
 
 var app = express();
 app.use(compression());
-
-app.set('view engine', 'html');
-
-app.engine('html', require('ejs').renderFile);
-
+app.use(cacheResponseDirective());
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
 
 var oneMonth = 2592000000;
 app.use('/dist/img', express.static(__dirname + '/dist/img', { maxAge: oneMonth }));
 app.use('/dist/css', express.static(__dirname + '/dist/css', { maxAge: oneMonth }));
 
-app.use('/dist/js', browserify(__dirname + '/dist/js/entry-points'));
+app.use('/dist/js', browserify(__dirname + '/dist/js/entry-points',  {cache: '1 month',  precompile: true}));
 
 app.get('/', function (req, res) {
-    res.redirect('/views/home');
+    renderPage(res, 'home', 'site');
 });
 
 app.get('/views/:name', function (req, res) {
@@ -43,6 +46,7 @@ app.get('/views/:name', function (req, res) {
 });
 
 app.get('/:pagina', function(req,res) {
+    res.cacheControl({maxAge: oneMonth});
     db['Equipe'].Model.findOne({ pagina: req.params.pagina}, function (err, value){
         if (value) {
             var logged, loggedOnPage = false;
@@ -63,30 +67,32 @@ app.get('/:pagina', function(req,res) {
 });
 
 app.get('/:name/find', function (req, res) {
-    var name = utils.capitalize(req.params.name);
-    var query = req.query;
+ res.cacheControl({maxAge: oneMonth});
+ var name = utils.capitalize(req.params.name);
+ var query = req.query;
 
-    var fields = query['fields'];
-    delete query['fields'];
+ var fields = query['fields'];
+ delete query['fields'];
 
-    db.findAll(db[name].Model, query, fields, function (err, values) {
-        if (err) {
-            res.status(404).send(err);
-        } else {
-            res.send(values);
-        }
-    });
+ db.findAll(db[name].Model, query, fields, function (err, values) {
+    if (err) {
+        res.status(404).send(err);
+    } else {
+        res.send(values);
+    }
+});
 });
 
 app.post('/:name/save', function (req, res) {
-    var name = utils.capitalize(req.params.name);
-    db.saveOrUpdate(db[name].Model, req.body, function (err, value) {
-        if (err) {
-            res.status(404).send(err);
-        } else {
-            res.send(value);
-        }
-    });
+ res.cacheControl({maxAge: oneMonth});
+ var name = utils.capitalize(req.params.name);
+ db.saveOrUpdate(db[name].Model, req.body, function (err, value) {
+    if (err) {
+        res.status(404).send(err);
+    } else {
+        res.send(value);
+    }
+});
 });
 
 
