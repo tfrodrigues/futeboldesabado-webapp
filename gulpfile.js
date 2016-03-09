57dc8a3583c54b137eb8e3190f598fb40c69984d
tfrodrigues <tfrodrigues@gmail.com>
2016-03-09 02:05:53 -0300

    Commit gulp production.

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task('start-dev', ['start-nodemon'], function() {
   gulp.watch([paths.html, paths.ejs, paths.targetContent]).on('change', browserSync.reload);
 });
 
-gulp.task('start-nodemon', function () {
+gulp.task('start-nodemon', function (cb) {
   var started = false;
   nodemon({
     script: 'server.js',
@@ -71,11 +71,15 @@ gulp.task('start-nodemon', function () {
 
 gulp.task('start-prd', function () {
   nodemon({
-    script: 'server.js', ext: 'js html ejs', env: { 'NODE_ENV': 'production' }
-  })
+    script: 'server.js',
+    ext: 'js html ejs',
+    env: { 'NODE_ENV': 'production' }
+  });
+  gulp.watch(paths.scss, ['sass']);
+  gulp.watch(paths.scripts, ['minify-scripts']);
   gulp.watch([paths.images, paths.uploads], ['minify-images']);
 })
 
 gulp.task('dev', ['sass', 'minify-scripts', 'minify-images', 'start-dev']);
 
-gulp.task('production', ['start-prd']);
+gulp.task('production', ['sass', 'minify-scripts', 'minify-images', 'start-prd']);
